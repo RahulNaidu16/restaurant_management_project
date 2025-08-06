fc53181e0de5293974beb8eaf73cfa693bc94e9e
RahulNaidu16 <21072cm035@gmail.com>
2025-08-06 20:20:25 +0530

    20880

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
-
+from django.db import models
+from django.contrib.auth.models import User
+class UserProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    phone_number=models.Charfield(max_length=15)
+def _str_(self):
+    return self.User.username
 
 
